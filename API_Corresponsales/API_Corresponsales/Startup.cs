2872e8ae8d58d52061ce21b13ec62bbb5dd67d11
adrivargas <adriandy2005@gmail.com>
2024-06-19 10:41:35 -0500

    API

---
+++ API_Corresponsales/API_Corresponsales/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace API_Corresponsales
+{
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configuraciones de middleware, manejo de excepciones, etc.
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers(); // Mapeo de los controladores
+        });
+    }
+
+}
